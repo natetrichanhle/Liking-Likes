c2efefeb0c6beaf61e5b70ad143e192e494da69f
Nate Trichanh <natetrichanh@gmail.com>
2021-11-16 14:48:56 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function change1 () {
+    var num = document.querySelector("#num1").innerText;
+    num = parseInt(num) + 1;
+    document.querySelector("#num1").innerText = num;
+}
+
+function change2 () {
+    var num = document.querySelector("#num2").innerText;
+    num = parseInt(num) + 1;
+    document.querySelector("#num2").innerText = num;
+}
+
+function change3 () {
+    var num = document.querySelector("#num3").innerText;
+    num = parseInt(num) + 1;
+    document.querySelector("#num3").innerText = num;
+}
